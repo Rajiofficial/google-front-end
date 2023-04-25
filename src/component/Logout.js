d68da1707992eb8a9fb207b15896ee3679a87a4a
demo <rajfernanto764@gmail.com>
2023-04-25 12:17:56 +0530

    first commit

--- src/component/Logout.js
+++ src/component/Logout.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+
 import React from 'react'
 import { GoogleLogout } from 'react-google-login';
 
@@ -7,7 +7,7 @@ const Logout = (props) => {
         console.log('logout successful')
         props.response()
 
-        axios.delete("http://localhost:3001/register")
+  
     }
     const clientId= "259846178726-jjdsbnhcbcvj6dndh9fdhofodku9srkh.apps.googleusercontent.com";
     return (
